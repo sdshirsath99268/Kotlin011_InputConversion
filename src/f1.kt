e609200bc4a1d530b2031b15d74d16e8a2303531
Sanjay Shirsath <sdshirsath@Sanjays-MacBook-Air.local>
2022-06-25 21:14:11 +0530

    Initial commit

---
+++ src/f1.kt
@@ -0,0 +1,20 @@
+fun main(args:Array<String>) {
+
+    /*
+    print("Enter Value for i : ")
+    val i = readLine()
+    print("Enter Value for j : ")
+    val j = readLine()
+
+    println(i+j)                  // Will be considered as string
+        */
+
+    print("Enter Value for i : ")
+    val i: Int = Integer.valueOf(readLine())   // Converted to Int
+    print("Enter Value for j : ")
+    val j: Int = Integer.valueOf(readLine())
+
+    println(i+j)
+
+    println("No : 11 Kotline Prog , By Sanjay D Shirsath")
+}
